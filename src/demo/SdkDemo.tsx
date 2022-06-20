05f05485b3005d8a9d9dbf748d277e9ecdf5b049
Warren Seine <warren.seine@onfido.com>
2022-06-20 17:02:39 +0200

    🩹 Stop requesting multiple tokens in demo app

--- src/demo/SdkDemo.tsx
+++ src/demo/SdkDemo.tsx
@@ -75,7 +75,7 @@ const SdkDemo: FunctionComponent<Props> = ({
 
     if (queryParamToValueString.token) {
       setToken(queryParamToValueString.token)
-    } else {
+    } else if (!token) {
       getToken(
         hasPreview,
         url,
